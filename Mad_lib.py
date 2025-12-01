be2e43deb43149c60ffadf21e57ad39bad5eec81
WinterCollinsSchool <winterc6@nycstudents.net>
2025-12-01 14:30:48 -0500

    Update Mad_lib.py

--- Mad_lib.py
+++ Mad_lib.py
@@ -1,13 +1,13 @@
-inport random
+import random
 
 verb = (input('Please enter a verb (action word): '))
 noun = (input('Please enter a noun (person, or animal): '))
 adjective = (input('Please enter a adjective (Descriptor.): '))
 
-story_nun=random.randint(1,0)
+story_nun=random.randint(1,2)
 
 if story_nun == 1:
 	print(f"The {adjective} {noun} jumped over the {verb} dog.")
 else:
-	print(f"The {adjective} {noun} {verb}")
+	print(f"The {adjective} {noun} is {verb}.")
 
