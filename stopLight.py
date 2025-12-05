4f3c15f0928ce97723aa27fe23708ffaf065667a
WinterCollinsSchool <winterc6@nycstudents.net>
2025-12-05 13:02:52 -0500

    Update stopLight.py

--- stopLight.py
+++ stopLight.py
@@ -4,7 +4,7 @@ from tkmacosx import Button
 
 # Create the main window
 root = Tk()
-root.title("Three Buttons Example")
+root.title("Stop Light")
 
 #Set size of window
 root.geometry("300x150")
@@ -13,17 +13,19 @@ root.geometry("300x150")
 red_button = Button(root, text="Red", background='red')
 yellow_button = Button(root,text="Yellow",background='yellow')
 green_button = Button(root, text="Green", background='green')
+light = Button(root, text="Color of LIGHT")
 #Add a label
-label = Label(root, text="This is a stoplight")
+label = Label(root, text="What color is the light?!")
 
-text_box = Text(root)
+text_box = Text(root,height=15,width=50)
 
 # Place widgets in window (with pack function!)
-red_button.pack()
-yellow_button.pack()
-green_button.pack()
-label.pack()
-text_box.pack()
+red_button.grid(row=0,column=0,padx=5)
+yellow_button.grid(row=0,column=1,padx=5)
+green_button.grid(row=0,column=2,padx=5)
+label.grid(row=1,column=1,pady=5)
+text_box.grid(row=2,column=1, columnspan=2,padx=5)
+light.grid(row=2,column=0,padx=5)
 
 # Start the GUI event loop
 root.mainloop()
